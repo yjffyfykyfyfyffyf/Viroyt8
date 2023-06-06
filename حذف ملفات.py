78a30be05deb9e30626376bde7e64238b6130a35
yjffyfykyfyfyffyf <133577457+yjffyfykyfyfyffyf@users.noreply.github.com>
2023-06-06 17:19:33 -0400

    Add files via upload

---
+++ حذف ملفات.py
@@ -0,0 +1,20 @@
+import os 
+
+print('''
+
+1 - حذفـ ملفاتي
+
+  2- لا سوي شي  
+   
+      ''')
+     
+o = input('inter : ')
+
+if o == ('1') :
+	f=os.rmdir('/sdcard/y')
+	print ('tm')
+else :
+	print ('no')
+	
+
+	
